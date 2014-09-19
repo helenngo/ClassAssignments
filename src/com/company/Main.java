c4c0a994f1929d05ae06a01e194d1b8c5bd8b32e
helenngo <Helen@148-100-141-182.foxnet.marist.edu>
2014-09-18 20:27:40 -0400

    Numbers 1-3
    The beginnings of the Worksheet: Review What We Learned.

---
+++ src/com/company/Main.java
@@ -0,0 +1,52 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) { //this is where it all begins
+
+    //#1
+	boolean isValid;
+    int numPeople = 17;
+    double avgScore = 14.3;
+    int[] peopleScore;
+    double[] avgSales = {16.3, 92.1, 101.4, 90.3}; //the length of this array is four, and the data type is an array of double
+    boolean[][] isUsed = new boolean[3][5]; //the left-hand side is just the data type
+        //by indexing as isUsed[1] then it is an array
+        //by indexing as isUsed[1][1] then it is a boolean
+
+    //#2
+    x++;
+    x-=2;
+    x*=2;
+    x=2*y;
+    myArray[4]=myOtherArray[0]; //but both arrays must be of the same kind
+
+        if (2 >= n*3){
+            x=y-1;
+        }
+
+    //#3
+
+        public static void CalcSomething(){
+            System.out.println("I like cats");
+        }
+            CalcSomething();
+
+        public static void DoSomeAction(int arg1,boolean arg2){
+            ...
+        } // We are passing an integer and a boolean. Remember to write the names.
+            DoSomeAction(12,false);
+
+        public static boolean IsEven(int agr3){
+            return num % 2==0;
+            0 == 0;
+        }
+            boolean x = IsEven(8);
+
+        public static double GetAverage (int[]arg4){
+            if (myArray[0] == 0);
+        }
+            int[] a = new int [6];
+            GetAverage(new int[]{1,2,3,4,5});
+    }
+}
